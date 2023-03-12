5c2d421d9160b4eb974236a3932967a14c0b8670
joshua <jsbyrd02@gmail.com>
2023-03-11 20:37:42 -0500

    Connect MongoDB database to Express.js server

--- server/app.js
+++ server/app.js
@@ -3,6 +3,18 @@ const app = express();
 
 const cors = require("cors");
 const mongoose = require("mongoose");
+require("dotenv").config();
+
+const url = process.env.MONGO_URI;
+
+mongoose
+  .connect(url)
+  .then((result) => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.log("Error connecting to MongoDB:", error.message);
+  });
 
 app.use(cors());
 app.use(express.json());
